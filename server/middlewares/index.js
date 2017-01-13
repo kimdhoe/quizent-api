978844766aeefa016246df11808f7bfb82557e09
kimdhoe <kimdhoe1@gmail.com>
2017-01-14 07:10:05 +0900

    chore

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -5,15 +5,11 @@ const morgan         = require('morgan')
 
 const config = require('../config')
 
-const corsOptions =
-  { origin:               'https://quizent-api.herokuapp.com'
-  , optionsSuccessStatus: 200
-  }
-
 const installMiddlewares = app => {
   app.use(bodyParser.urlencoded({extended: true}))
   app.use(bodyParser.json())
-  app.use(cors(corsOptions))
+  app.options('*', cors())
+  app.use(cors())
   app.use(methodOverride())
   app.use(morgan(config.morgan))
 }
